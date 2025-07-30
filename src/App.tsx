d8ea4b92fe51ccbecff1a5190e395d30c26c25a2
Tamilarasan117 <tamilarasan31403@gmail.com>
2025-07-30 09:45:22 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className='bg-black flex items-center justify-center min-h-[100vh] p-0'>
+      <h1 className='text-white text-2xl font-bold'>Memorable Invite!</h1>
+    </div>
+  )
+}
+
+export default App
