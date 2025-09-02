d0a6e84a8acc65306eb4e38683f2becfa7400402
Tamilarasan117 <tamilarasan31403@gmail.com>
2025-09-02 16:19:16 +0530

    bug fixes

--- frontend/src/components/CoupleIntro.tsx
+++ frontend/src/components/CoupleIntro.tsx
@@ -134,7 +134,7 @@ const CoupleIntro = () => {
         className="absolute bottom-[10%] right-[10%] w-2 animate-float3 z-10"
       />
 
-      <div className="relative w-full max-w-[600px] h-[300px] mx-auto mb-12">
+      <div className="relative w-full max-w-[600px] h-[350px] mx-auto mb-12">
         <div className="absolute top-0 left-0">
           <CoupleCard
             backgroundPosition="left"
